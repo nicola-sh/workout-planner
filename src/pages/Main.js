e9c86335be43f96e7e6aa1bbb85e6ce3bde2b776
Nicola <shirnin.kolia@gmail.com>
2023-11-09 01:38:38 +0300

    add list of days

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,3 @@
-import React from "react";
 import styled from "styled-components";
 
 export const MainContainer = styled.main`
@@ -13,10 +12,25 @@ export const MainContainer = styled.main`
   min-height: 600px;
 `;
 
+import React from "react";
+import { useState } from "react";
+
 const Main = () => {
+  const [days, setDays] = useState([
+    { TimeToStart: "Сегодня", DayName: "День А", id: 1 },
+    { TimeToStart: "Через 2 дня", DayName: "День Б", id: 2 },
+    { TimeToStart: "Через 4 дня", DayName: "День Рук", id: 3 },
+    { TimeToStart: "Через неделю", DayName: "День Ног", id: 4 },
+  ]);
+
   return (
     <MainContainer>
-      <h1>Main</h1>
+      {days.map((day) => (
+        <div className="day-preview" key={day.id}>
+          <h3>{day.TimeToStart}</h3>
+          <h4>{day.DayName}</h4>
+        </div>
+      ))}
     </MainContainer>
   );
 };
